54fc968dbf5bc7e31b7fac511c929f92d3074359
ethancowdery12-spec <ethancowdery12@gmail.com>
2026-09-20 23:35:46 -0500

    feat: add humanize to controller tool allowlist

--- src/noesek/core/orchestration.py
+++ src/noesek/core/orchestration.py
@@ -27,7 +27,7 @@ CONTROLLER_TOOLS = frozenset({
     "create_task", "list_tasks", "cancel_task",
     "delegate_task",
     "gmail_read", "gmail_send", "calendar_read", "github_notifications",
-    "create_file", "speak", "optimize_prompt",
+    "create_file", "speak", "optimize_prompt", "humanize",
 })
 
 
